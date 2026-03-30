49ac640bdcb98043916a75744c3378ac7ace1e9d
Jim-Lii <343347795@tdsb.ca>
2026-03-30 16:05:46 +0000

    fix

--- src/main/java/ProblemSet.java
+++ src/main/java/ProblemSet.java
@@ -2,24 +2,26 @@
 File Name: Problem Set Unit 3 (100%)
 Author: Jim Li
 Date Created: Mar. 27, 2026
-Date Last Modified: Mar. 28, 2026
+Date Last Modified: Mar. 30, 2026
 */
 
 import java.util.Scanner;
 public class ProblemSet {
 
 	public static void main(String[] args) {
-		//receives then parses 2 emails
+		//input emails
 		Scanner input = new Scanner(System.in);
 		System.out.print("Input two emails: ");
-		String email = input.nextLine();
-		email = email.toLowerCase(); //remove case sensitivity
+		String emails = input.nextLine();
+		emails = emails.toLowerCase(); //remove case sensitivity
 
-		if (!email.contains(",")) {
+		if (!emails.contains(",")) {
 			System.out.println("Separate emails with a comma.");
 		}
-		String email1 = (email.substring(0, email.indexOf(",")));
-		String email2 = (email.substring(email.indexOf(",") + 2));
+
+		//parse emails
+		String email1 = (emails.substring(0, emails.indexOf(",")));
+		String email2 = (emails.substring(emails.indexOf(",") + 2));
 
 		//validation first attempt
 		String reason1 = validate(email1);
@@ -30,34 +32,10 @@ public class ProblemSet {
 		reason2 = exceptionCheck(email2, reason2);
 
         //final outputs
-		//email 1 output
-		if (reason1.equals("Valid")) { //if valid
-		    String local1 = email1.substring(0, email1.indexOf("@")); //extract local
-	    	String domain1 = email1.substring(email1.indexOf("@") + 1); //extract domain
-			System.out.print(email1 + ": Valid "); //print "email: valid "
-			if (domain1.equals("gmail.com")) { //if gmail, print "(gmail normalized) "
-				System.out.print("(Gmail normalized) ");
-			}
-			System.out.println("| Local: " + local1 + " | Domain: " + domain1); //print local + domain
-		} else { //if not valid
-			System.out.println(email1 + ": Invalid: " + reason1); //print email: invalid: reason
-		}
-		
-		//email 2 output
-		if (reason2.equals("Valid")) {
-		    String local2 = email2.substring(0, email.indexOf("@"));
-	    	String domain2 = email2.substring(email.indexOf("@") + 1);
-			System.out.print(email2 + ": Valid ");
-			if (domain2.equals("gmail.com")) {
-				System.out.print("(Gmail normalized) ");
-			}
-			System.out.println("| Local: " + local2 + " | Domain: " + domain2);
-		} else {
-			System.out.println(email2 + ": Invalid: " + reason2);
-		}
-
+        System.out.println(result(reason1, email1));
+        System.out.println(result(reason2, email2));
 	}
-	
+
 	//validation method
 	public static String validate(String email) {
 		if (!email.contains("@")) { //step 1.1 - contains @?
@@ -91,30 +69,32 @@ public class ProblemSet {
 	
 	//exception checks, combined
 	public static String exceptionCheck(String email, String reason) {
-	    //exception A
+	    //exception A - only applies if original reason was invalid and multiple dots in domain
 		if (!reason.equals("Valid")) {
 		    String domain = email.substring(email.indexOf("@") + 1);
 		    if (domain.indexOf(".") != domain.lastIndexOf(".")){ //checks for multiple dots
-			    reason = exceptionA(email); //reattempt validation
+			    reason = validate(exceptionA(email)); //reattempt validation
 		    }
 		}
-		//exception B
-		if (reason.equals("Valid")) { //secondary check for exception B specific rules
+		//exception B - only applies if original attempt was valid (second check)
+		boolean exceptionB = false;
+		if (reason.equals("Valid")) {
 			reason = exceptionB(email);
+			if (!reason.equals("Valid")){
+			    exceptionB = true; //gmail was cancelling exception B so I added this
+			}
 		}
-		//exception C
-		if (!reason.equals("Valid")) {
+		//exception C - only applies if domain is gmail.com and exception B was valid
+		if (!reason.equals("Valid") && exceptionB == false) {
 		    String domain = email.substring(email.indexOf("@") + 1);
 			if (domain.equals("gmail.com")) {
-				email = exceptionC(email); //if domain is gmail, remove dots from local
-				reason = validate(email); //reattempt validation
+				reason = validate(exceptionC(email)); //remove dots and reattempt validation
 			}
 		}
 		return reason;
 	}
 
 	//exception A - removes all dots in the domain (except the final one) then revalidates 
-	//only applies if domain contains multiple dots
 	public static String exceptionA(String email) {
 	    String local = email.substring(0, email.indexOf("@"));
 	    String domain = email.substring(email.indexOf("@") + 1);
@@ -126,39 +106,43 @@ public class ProblemSet {
 	}
 
 	//exception B - works by checking email beginning and domain extension for stuff
-	//only applies if original attempt was valid
 	public static String exceptionB(String email) {
-		if (email.startsWith("+") || email.startsWith("_")) {
-			return "Starts with plus or underscore";
+		if (email.startsWith("+") || email.endsWith("+")) { //starts or ends with plus?
+			return "Starts or ends with plus";
+		} else if (email.startsWith("_") || email.endsWith("_")) { //starts or ends with underscore?
+			return "Starts or ends with underscore";
 		} else {
 		    String domain = email.substring(email.indexOf("@") + 1);
-			String domainExtension = domain.substring(domain.indexOf(".")); //extract domain extension
-			if (containsNonLetter(domainExtension)) {
-				return "Domain extension contains non-letters";
+			if (domain.contains("+")) { //domain contains plus?
+				return "Domain contains plus";
+			} else if (domain.contains("_")){ //domain contains underscore?
+			    return "Domain contains underscore";
 			} else {
-				return "Valid";
+				return "Valid"; //valid if all conditions passed
 			}
 		}
 	}
 
 	//exception C - removes dots from local
-	//only applies if domain is gmail.com
 	public static String exceptionC(String email) {
 	    String local = email.substring(0, email.indexOf("@"));
 	    String domain = email.substring(email.indexOf("@") + 1);
 		local = local.replace(".", "");
 		return local + "@" + domain;
 	}
-
-	// method for detecting non-letters (+, _, 0-9); domain extension abbreviated to dE
-	public static boolean containsNonLetter(String dE) {
-		if (dE.contains("+") || dE.contains("_") || dE.contains("0") ||
-		        dE.contains("1") || dE.contains("2") || dE.contains("3") ||
-		        dE.contains("4") || dE.contains("5") || dE.contains("6") ||
-		        dE.contains("7") || dE.contains("8") || dE.contains("9")) {
-			return true;
-		} else {
-			return false;
+	
+	//result method
+	public static String result(String reason, String email){
+	    	if (reason.equals("Valid")) {
+		    String local = email.substring(0, email.indexOf("@"));
+	    	String domain = email.substring(email.indexOf("@") + 1);
+			System.out.print(email + ": Valid ");
+			if (domain.equals("gmail.com")) {
+				System.out.print("(Gmail normalized) ");
+			}
+			return "| Local: " + local + " | Domain: " + domain;
+		    } else {
+		    	return email + ": Invalid: " + reason;
 		}
 	}
 }
